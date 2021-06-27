c7477914f56afd5590bd229bf51053a1ca05d1e5
zhanggl94 <zhanggl94@163.com>
2021-06-27 21:58:16 +0800

    update: 修改sidebar的样式

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,89 +8,80 @@
 -->
 
 <template>
-  <div>
-    <el-menu
-      default-active="1"
-      background-color="#324157"
-      text-color="#bfcbd9"
-      active-text-color="#20a0ff"
-      unique-opened
-    >
-      <template v-for="item in sidbarData">
-        <template v-if="item.subs">
-          <el-submenu :index="item.index" :key="item.index">
-            <template #title>
-              <i :class="item.icon"></i>
-              <span>{{ item.title }}</span>
-              <template v-for="subItem in item.subs">
-                <el-submenu
-                  v-if="subItem.subs"
-                  :index="subItem.index"
-                  :key="subItem.index"
-                >
-                  <template #title>{{ subItem.title }}</template>
-                  <el-menu-item
-                    v-for="(threeItem, i) in subItem.subs"
-                    :index="threeItem.index"
-                    :key="i"
-                  >
-                    {{ threeItem.title }}
-                  </el-menu-item>
-                </el-submenu>
-                <el-submenu v-else :index="subItem.index" :key="subItem.index">
-                  <span>{{ subItem.title }}</span>
-                </el-submenu>
-              </template>
+    <div>
+        <el-menu default-active="1" background-color="#324157" text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened>
+            <template v-for="item in sidbarData">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index" :key="item.index">
+                        <template #title>
+                            <i :class="item.icon"></i>
+                            <span>{{ item.title }}</span>
+                            <template v-for="subItem in item.subs">
+                                <template v-if="subItem.subs">
+                                    <el-submenu :index="subItem.index" :key="subItem.index">
+                                        <template #title>{{ subItem.title }}</template>
+                                        <el-menu-item v-for="(threeItem, i) in subItem.subs" :index="threeItem.index" :key="i">
+                                            {{ threeItem.title }}
+                                        </el-menu-item>
+                                    </el-submenu>
+                                </template>
+                                <template v-else>
+                                    <el-submenu :index="subItem.index" :key="subItem.index">
+                                        <span>{{ subItem.title }}</span>
+                                    </el-submenu>
+                                </template>
+                            </template>
+                        </template>
+                    </el-submenu>
+                </template>
+                <template v-else> </template>
             </template>
-          </el-submenu>
-        </template>
-        <template v-else> </template>
-      </template>
-      <!-- <el-submenu index="">
+            <!-- <el-submenu index="">
         <el-menu-item index=""></el-menu-item>
       </el-submenu>
       <el-menu-item index=""></el-menu-item> -->
-    </el-menu>
-  </div>
+        </el-menu>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      sidbarData: [
-        {
-          icon: 'el-icon-warning-outline',
-          index: 'error',
-          title: '错误处理',
-          subs: [
-            {
-              index: 'permission',
-              title: '权限测试',
-            },
-            {
-              index: 'error-404',
-              title: '404',
-            },
-          ],
-        },
-        {},
-      ],
+    data() {
+        return {
+            sidbarData: [
+                {
+                    icon: 'el-icon-warning-outline',
+                    index: 'error',
+                    title: '错误处理',
+                    subs: [
+                        {
+                            index: 'permission',
+                            title: '权限测试'
+                        },
+                        {
+                            index: 'error-404',
+                            title: '404'
+                        }
+                    ]
+                },
+                {}
+            ]
+        }
     }
-  },
 }
 </script>
 
 <style>
 .sidebar {
-  display: block;
-  /* position: absolute; */
-  left: 0;
-  /* top: 70px; */
-  bottom: 0;
-  overflow-y: scroll;
+    display: block;
+    /* position: absolute; */
+    left: 0;
+    /* top: 70px; */
+    bottom: 0;
+    overflow-y: scroll;
+    height: 100%;
 }
 .sidebar > ul {
-  height: 100%;
+    height: 100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,26 +8,26 @@
 -->
 
 <template>
-  <div>
-    <v-header></v-header>
-    <div class="middle">
-      <div class="sidebar">
-        <v-sidebar></v-sidebar>
-      </div>
-      <div class="content-box flex-weight1">
-        <v-tags></v-tags>
-        <div class="content">
-          <router-view v-slot="{ Component }">
-            <transition name="move" mode="out-in">
-              <keep-alive :include="tagList">
-                <component :is="Component"></component>
-              </keep-alive>
-            </transition>
-          </router-view>
+    <div class="home">
+        <v-header></v-header>
+        <div class="middle">
+            <div class="sidebar">
+                <v-sidebar></v-sidebar>
+            </div>
+            <div class="content-box flex-weight1">
+                <v-tags></v-tags>
+                <div class="content">
+                    <router-view v-slot="{ Component }">
+                        <transition name="move" mode="out-in">
+                            <keep-alive :include="tagList">
+                                <component :is="Component"></component>
+                            </keep-alive>
+                        </transition>
+                    </router-view>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -37,32 +37,35 @@ import vSidebar from '../components/Sidebar.vue'
 import vTags from '../components/Tags'
 
 export default {
-  components: {
-    vHeader,
-    vSidebar,
-    vTags,
-  },
-  computed: {
-    ...mapState({
-      tagList: (state) => state.tags.tagList.map((item) => item.name),
-    }),
-  },
+    components: {
+        vHeader,
+        vSidebar,
+        vTags
+    },
+    computed: {
+        ...mapState({
+            tagList: state => state.tags.tagList.map(item => item.name)
+        })
+    }
 }
 </script>
 
 
 <style scoped>
+.home {
+    height: 100%;
+}
 .middle {
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: space-between;
-  height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 100%;
 }
 .sidebar {
-  width: 250px;
+    width: 250px;
 }
 .flex-weight1 {
-  flex-grow: 1;
+    flex-grow: 1;
 }
 </style>
