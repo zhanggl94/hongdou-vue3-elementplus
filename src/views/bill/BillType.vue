4715db207b0a4e1164994442e5a63b4e0d215078
zhanggl94 <zhanggl94@163.com>
2021-07-03 00:11:11 +0800

    add: 添加账单类型

--- src/views/bill/BillType.vue
+++ src/views/bill/BillType.vue
@@ -1,5 +1,43 @@
+<!--
+ * @Descripttion: 
+ * @Version: 
+ * @Author: zhanggl
+ * @Date: 2021-07-02 22:17:44
+ * @LastEditors: zhanggl
+ * @LastEditTime: 2021-07-02 23:56:03
+-->
 <template>
-    <div>
-        BillType
+  <div>
+    <el-breadcrumb class="crumbs" separator="/">
+      <el-breadcrumb-item>
+        <i class="el-icon-truck"></i> 汽车账单
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>账单类型</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="container">
+      <template>
+        <el-form :modle="form">
+          <el-form-item label="类型">
+            <el-input type="input" v-model="form.type"></el-input>
+          </el-form-item>
+          <el-form-item label="备注">
+              <el-input type="textarea" :rows="3" v-model="form.note"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
     </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        type: '',
+        note: '',
+      },
+    }
+  },
+}
+</script>
