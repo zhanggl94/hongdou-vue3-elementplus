3f94a0ccae5e17d8533a63e5f1495467f8bd9680
zhanggl <zhanggl94@163.com>
2021-07-09 10:18:13 +0800

    add: 添加跨域处理

--- src/api/base.js
+++ src/api/base.js
@@ -4,11 +4,12 @@
  * @Autor: zhanggl
  * @Date: 2021-07-08 14:28:11
  * @LastEditors: zhanggl
- * @LastEditTime: 2021-07-08 14:28:12
+ * @LastEditTime: 2021-07-09 10:17:58
  */
 
 export default {
-    management: 'http://localhost:3001/api',
+    // management: 'http://localhost:3001/api',
+    management: '/api', // 开发环境下，启用跨域时的baseurl
     signin: '/signin', // 登陆
     signup: '/signup', // 注册
 }
--- vue.config.js
+++ vue.config.js
@@ -4,20 +4,29 @@
  * @Autor: zhanggl
  * @Date: 2021-07-08 17:02:51
  * @LastEditors: zhanggl
- * @LastEditTime: 2021-07-08 17:02:59
+ * @LastEditTime: 2021-07-09 10:17:11
  */
 module.exports = {
-    lintOnSave: false,
-    devServer: {
-      //开发环境下设置为编译好以后直接打开浏览器浏览
-      open: true,
-    },
-    configureWebpack: (config) => {
-      //调试JS
-      config.devtool = "source-map"
-    },
-    css: {
-      //查看CSS属于哪个css文件
-      sourceMap: true,
-    },
-  }
+  lintOnSave: false,
+  devServer: {
+    //开发环境下设置为编译好以后直接打开浏览器浏览
+    open: true,
+    // 代理设置，解决开发环境下的跨域问题
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001/',  		// 要请求的域名(这里指的是hongdou-node工程的地址)
+        pathRewrite: { '^/api': '/api' },			// 通过pathRewrite重写地址，将前缀/api转为/api（因为hongdou-node的路由中包括/api，所以此处也是/api）
+        ws: false,							// 是否启用websockets
+        changeOrigin: true					//开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+      }
+    }
+  },
+  configureWebpack: (config) => {
+    //调试JS
+    config.devtool = "source-map"
+  },
+  css: {
+    //查看CSS属于哪个css文件
+    sourceMap: true,
+  },
+}
