9bd4498755a9d0605ca2ccf9df287c61d159a017
zhanggl <zhanggl94@163.com>
2021-07-26 16:39:05 +0800

    update: 修改ElementPlus分页组件显示英文的问题

--- src/plugins/elementplus.js
+++ src/plugins/elementplus.js
@@ -4,12 +4,13 @@
  * @Author: zhanggl
  * @Date: 2021-06-18 21:44:58
  * @LastEditors: zhanggl
- * @LastEditTime: 2021-06-19 06:48:44
+ * @LastEditTime: 2021-07-26 16:38:22
  */
 
 import ElementPlus from 'element-plus'
+import locale from 'element-plus/lib/locale/lang/zh-cn'  //element-plus
 import 'element-plus/lib/theme-chalk/index.css'
 
 export default app => {
-  app.use(ElementPlus)
+  app.use(ElementPlus, { locale })
 }
